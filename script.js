6dce6680e8ef99be03ce9ef6f84195df8390c10f
ZihadDomain <zihadmia5731@gmail.com>
2024-05-07 17:04:25 +0600

    making a website

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ console.log(arrNew);
 for(var i =0;i<=arrNew.length;i++){
     console.log(arrNew);
 }
+var arrNew3=[2,0,3,2,8]
+console.log(arrNew);
+for(var i =0;i<=arrNew.length;i++){
+    console.log(arrNew);
+}
